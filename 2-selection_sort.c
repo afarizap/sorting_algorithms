ec555b251f0f9b25bc624f66fa93c3ea44b49241
andres reyes <andressantiagore@gmail.com>
2020-06-12 16:43:15 -0500

    cambios a la mitad en el punto 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,20 @@
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+void selection_sort(int *array, size_t size)
+{
+	int limit = 0;
+
+	if (size <= 2)
+		return;
+
+	for(i = 0;)
+	{
+		if(i == 2)
+
+	}
+}
