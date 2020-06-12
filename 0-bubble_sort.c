9072538428ac29790a0fe630be3da477525f1cda
afarizap <afarizap@gmail.com>
2020-06-12 13:51:45 +0000

    0 task working without checker

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,7 +8,15 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	
+	size_t i = 0, j = 0;
 
-
+	for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
+			if (array[j] > array[j+1])
+			{
+				array[j] = array[j] ^ array[j + 1];
+				array[j + 1] = array[j] ^ array[j + 1];
+				array[j] = array[j] ^ array[j + 1];
+				print_array(array, size);
+			}
 }
