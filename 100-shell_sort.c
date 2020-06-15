e76edf63d7f5d8195c1a1c609d7b0c97122a84bb
afarizap <afarizap@gmail.com>
2020-06-15 19:07:45 +0000

    add shell and .h

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,10 +1,11 @@
 #include "sort.h"
 
-/*You must use the following sequence of intervals (a.k.a the Knuth sequence)*/
-/*
+/**
+ *You must use the following sequence of intervals (a.k.a the Knuth sequence)
+ *
  *    n+1  = n * 3 + 1
  *    1, 4, 13, 40, 121, ...
-*/
+ */
 
 void swap(int *array, int dest, int origin);
 /**
@@ -17,15 +18,14 @@ void swap(int *array, int dest, int origin);
  */
 void shell_sort(int *array, size_t size)
 {
-	int gap = 1, i = 0, j = 0;
-	/*Found the gap*/
+	int gap, i = 0, j = 0, knut = 0;
+	/*Find the gap*/
 	if (size < 2 || array == NULL)
 		return;
-
-	while ((gap * 3 + 1) < (int) size)
-		gap = (gap * 3 + 1);
-
-	while (gap >= 1)
+	while ((knut * 3) + 1 < (int)size)
+		knut = (knut * 3) + 1;
+	gap = knut;
+	while (gap > 1)
 	{
 		j = (size - 1) - gap, i = size - 1;
 		while (j >= 0)
@@ -35,7 +35,20 @@ void shell_sort(int *array, size_t size)
 			j--, i--;
 		}
 		gap = (gap - 1) /3;
+		print_array(array, size);
 	}
+	j = (size - 1) - gap, i = size - 1;
+	while (i >= 0)
+	{
+		if (array[i] < array[j])
+		{
+			swap(array, i, j);
+			j = (size - 1) - gap, i = size - 1;
+		}
+		else
+			j--, i--;
+	}
+	print_array(array, size);
 }
 
 void swap(int *array, int dest, int origin)
--- sort.h
+++ sort.h
@@ -34,5 +34,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 listint_t swapdll(listint_t **list);
+void shell_sort(int *array, size_t size);
 
 #endif
