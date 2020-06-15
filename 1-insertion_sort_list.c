04280fdc56f4cff768b3e748d67b080a18aa3c62
andres reyes <andressantiagore@gmail.com>
2020-06-15 00:05:35 +0000

    improve the read into the code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -17,33 +17,33 @@ void put_linkedk(listint_t *move_node)
 }
 
 /**
- * create_listint - Creates a doubly linked list from an array of integers
- * @array: Array to convert to a doubly linked list
+ * insertion_sort_list - Creates a doubly linked list from an array of integers
+ * @list: list to convert to a doubly linked list
  *
  * Return: Pointer to the first element of the created list. NULL on failure
  */
 void insertion_sort_list(listint_t **list)
 {
-     listint_t *move_node = NULL;
+	listint_t *move_node = NULL;
 
-     if (*list == NULL)
-     	return;
+	if (*list == NULL)
+		return;
 
 	move_node = (*list)->next;
-     while (move_node != NULL || (*list)->next == NULL)
-     {
-          while (move_node->prev != NULL
+	while (move_node != NULL || (*list)->next == NULL)
+	{
+		while (move_node->prev != NULL
 		&& move_node->n < move_node->prev->n)
-          {
+		{
 			/* moverse pa tras*/
 			put_linkedk(move_node);
-               if (move_node->prev == NULL)
-                    *list = move_node;
-               else
-                    move_node->prev->next = move_node;
-               print_list(*list);
-          }
+			if (move_node->prev == NULL)
+				*list = move_node;
+			else
+				move_node->prev->next = move_node;
+			print_list(*list);
+		}
 		/* move forward*/
-          move_node = move_node->next;
-     }
+		move_node = move_node->next;
+	}
 }
