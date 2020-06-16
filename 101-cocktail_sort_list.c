cc8b3c5fc1a59079dad1935ff2633fe82edd9104
afarizap <afarizap@gmail.com>
2020-06-16 16:13:12 +0000

    cocktail betty and if list null

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,26 +1,22 @@
 #include "sort.h"
 /**
- * cocktail_sort_list - sorts a dllist of int using the
- * Cocktail shaker sort algorithm
+ * cocktail_sort_list - sorts a dllist with Cocktail shaker sort algorithm
  * @list: ddl
  */
 void cocktail_sort_list(listint_t **list)
 {
-        int i = 0, j = 0;
-        listint_t *aux1, *aux;
+	int i = 0, j = 0;
+	listint_t *aux1 = *list, *aux = *list;
 
-        aux1 = *list;
-        aux = *list;
-        if (aux1)
-        {
+	if (list == NULL)
+		return;
+	if (aux1)
 		for (; aux1->next; j++)
 			aux1 = aux1->next;
-		aux1 = NULL;
-        }
-        else
+	else
 		return;
-        for (; j > 0; j--)
-        {
+	for (; j > 0; j--)
+	{
 		i = j;
 		for (; j > 0; j--)
 		{
@@ -37,76 +33,73 @@ void cocktail_sort_list(listint_t **list)
 			if (aux->prev->n > aux->n)
 			{
 				bbl_down(aux);
-				if (aux->n < (*list)->n && i - j <2)
+				if (aux->n < (*list)->n && i - j < 2)
 					*list = aux;
 				print_list(*list);
 			}
 			else
-			{
 				aux = aux->prev;
-			}
 		}
-		/* *list = aux; */
 	}
 }
 /**
- * put_linkedk - change the nodes backward
- * @move_node: The node it will change
+ * bbl_up - change the nodes foward
+ * @pointer: The node it will change
  */
 void bbl_up(listint_t *pointer)
 {
-        listint_t *low;
+	listint_t *low;
 
-        low = pointer;
-        if (pointer->next)
-                low = low->next;
-        else
+	low = pointer;
+	if (pointer->next)
+		low = low->next;
+	else
 		return;
-        if (pointer->prev)
-        {
+	if (pointer->prev)
+	{
 		low->prev = pointer->prev;
 		pointer->prev->next = low;
-        }
-        else
+	}
+	else
 		low->prev = NULL;
-        if (low->next)
-        {
+	if (low->next)
+	{
 		pointer->next = low->next;
 		low->next->prev = pointer;
-        }
-        else
+	}
+	else
 		pointer->next = NULL;
-        pointer->prev = low;
-        low->next = pointer;
+	pointer->prev = low;
+	low->next = pointer;
 
 }
 /**
- * put_linkedk - change the nodes backward
- * @move_node: The node it will change
+ * bbl_down - change the nodes backward
+ * @pointer: The node it will change
  */
 void bbl_down(listint_t *pointer)
 {
-        listint_t *big;
+	listint_t *big;
 
-        big = pointer;
-        if (pointer->prev)
-                big = big->prev;
-        else
+	big = pointer;
+	if (pointer->prev)
+		big = big->prev;
+	else
 		return;
-        if (pointer->next)
-        {
+	if (pointer->next)
+	{
 		big->next = pointer->next;
 		pointer->next->prev = big;
-        }
-        else
+	}
+	else
 		big->next = NULL;
-        if (big->prev)
-        {
+	if (big->prev)
+	{
 		pointer->prev = big->prev;
 		big->prev->next = pointer;
-        }
-        else
+	}
+	else
 		pointer->prev = NULL;
-        pointer->next = big;
-        big->prev = pointer;
+	pointer->next = big;
+	big->prev = pointer;
 }
